26ec804f2100a83a747216f4e402d8ac388a89dd
GardyVoir <clement.wilfart@gmail.com>
2023-02-26 22:02:16 +0100

    feat: :sparkles: Début de la page pokémon
    Rédaction de la page pokémon et mise a jour de la main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:clefairy/pages/Carte.dart';
-import 'package:clefairy/pages/Pokemon.dart';
+import 'package:clefairy/pages/pokemon.dart';
 import 'package:clefairy/pages/Attaques.dart';
 import 'package:clefairy/pages/Statistiques.dart';
 import 'package:clefairy/pokedex_frames.dart';
 import 'package:clefairy/services/pokemon_service.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import 'api/app_interceptors.dart';
+import 'models/pokemon.dart';
 
 void main() {
   runApp(const MyApp());
@@ -40,72 +44,89 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  final GlobalKey<ScaffoldMessengerState> scaffoldKey = GlobalKey<ScaffoldMessengerState>();
+  Pokemon pokemon = Pokemon();
+
   @override
   Widget build(BuildContext context) {
     final Size size = MediaQuery.of(context).size;
-    final List<Widget> pages = [
-      const Pokemon(),
+    List<Widget> pages = [
+      PokemonPage(pokemon: pokemon),
       const Attaques(),
       const Carte(),
       const Statistiques(),
     ];
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          resizeToAvoidBottomInset: false,
-          body: Stack(children: [
-            Positioned(
-                top: 0,
-                child: Stack(children: [
-                  CustomPaint(size: Size(size.width, 80), painter: TopFrame()),
-                ])),
-            Positioned.fill(
-                top: 30,
-                left: 10,
-                right: 110,
-                child: TextField(
-                  decoration: const InputDecoration(
-                      fillColor: Colors.white54,
-                      filled: true,
-                      prefixIcon: Icon(Icons.search),
-                      hintText: "Rechercher un Pokémon...",
-                      isDense: false,
-                      contentPadding: EdgeInsets.all(0),
-                      border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)))),
-                  onChanged: (text) {},
-                  onSubmitted: (text) async {
-                    var pokemon = await PokemonService().getPokemon(text);
-                  },
-                )),
-            Positioned(child: Align(alignment: Alignment.center, child: pages.elementAt(_currentIndex))),
-            Positioned(
-                bottom: 0,
-                child: Stack(
-                  children: [CustomPaint(size: Size(size.width, 160), painter: BottomFrame())],
-                )),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: SizedBox(
-                height: 70,
-                child: BottomNavigationBar(
-                  backgroundColor: Colors.transparent,
-                  selectedItemColor: Colors.white,
-                  elevation: 0,
-                  showSelectedLabels: true,
-                  onTap: onTabTapped,
-                  currentIndex: _currentIndex,
-                  type: BottomNavigationBarType.fixed,
-                  iconSize: 20,
-                  items: const [
-                    BottomNavigationBarItem(icon: Icon(Icons.sports_baseball_outlined), label: 'Pokémon'),
-                    BottomNavigationBarItem(icon: Icon(Icons.ac_unit_sharp), label: 'Attaques'),
-                    BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: 'Carte'),
-                    BottomNavigationBarItem(icon: Icon(Icons.stacked_bar_chart), label: 'Statistiques'),
-                  ],
-                ),
+    return ScaffoldMessenger(
+      key: scaffoldKey,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: Stack(children: [
+          Positioned(
+              top: 0,
+              child: Stack(children: [
+                CustomPaint(size: Size(size.width, 80), painter: TopFrame()),
+              ])),
+          Positioned.fill(
+              top: 30,
+              left: 10,
+              right: 110,
+              child: TextField(
+                decoration: const InputDecoration(
+                    fillColor: Colors.white54,
+                    filled: true,
+                    prefixIcon: Icon(Icons.search),
+                    hintText: "Rechercher un Pokémon...",
+                    isDense: false,
+                    contentPadding: EdgeInsets.all(0),
+                    border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(10.0)))),
+                onChanged: (text) {},
+                onSubmitted: (text) async {
+                  try {
+                    var result = await PokemonService().getPokemon(text.trim().toLowerCase());
+                    setState(() {
+                      pokemon = result;
+                    });
+                  } on DioError catch (_) {
+                    scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                      content: Text("Le pokémon est introuvable"),
+                    ));
+                  } catch (_) {
+                    scaffoldKey.currentState?.showSnackBar(const SnackBar(
+                      content: Text("Une erreur s'est produite"),
+                    ));
+                  }
+                },
+              )),
+          Positioned(child: Align(alignment: Alignment.center, child: pages.elementAt(_currentIndex))),
+          Positioned(
+              bottom: 0,
+              child: Stack(
+                children: [CustomPaint(size: Size(size.width, 160), painter: BottomFrame())],
+              )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: 70,
+              child: BottomNavigationBar(
+                backgroundColor: Colors.transparent,
+                selectedItemColor: Colors.white,
+                elevation: 0,
+                showSelectedLabels: true,
+                onTap: onTabTapped,
+                currentIndex: _currentIndex,
+                type: BottomNavigationBarType.fixed,
+                iconSize: 20,
+                items: const [
+                  BottomNavigationBarItem(icon: Icon(Icons.sports_baseball_outlined), label: 'Pokémon'),
+                  BottomNavigationBarItem(icon: Icon(Icons.ac_unit_sharp), label: 'Attaques'),
+                  BottomNavigationBarItem(icon: Icon(Icons.map_outlined), label: 'Carte'),
+                  BottomNavigationBarItem(icon: Icon(Icons.stacked_bar_chart), label: 'Statistiques'),
+                ],
               ),
             ),
-          ]),
-        ));
+          ),
+        ]),
+      ),
+    );
   }
 }
--- lib/pages/Pokemon.dart
+++ lib/pages/Pokemon.dart
@@ -1,15 +1,102 @@
 import 'package:flutter/material.dart';
 
-class Pokemon extends StatefulWidget {
-  const Pokemon({super.key});
+import '../models/pokemon.dart';
+
+class PokemonPage extends StatefulWidget {
+  final Pokemon pokemon;
+  const PokemonPage({super.key, required this.pokemon});
 
   @override
-  State<Pokemon> createState() => _PokemonState();
+  State<PokemonPage> createState() => _PokemonPageState();
 }
 
-class _PokemonState extends State<Pokemon> {
+extension StringExtension on String? {
+  String capitalize() {
+    return this == null ? "" : "${this!.substring(0, 1).toUpperCase()}${this!.substring(1)}";
+  }
+}
+
+class _PokemonPageState extends State<PokemonPage> {
   @override
   Widget build(BuildContext context) {
-    return Text("POKEMON");
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Flexible(
+              child: Container(
+                constraints: const BoxConstraints(maxWidth: 200),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            Text(
+                              widget.pokemon.name.capitalize(),
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            Text(
+                              "N°${widget.pokemon.id != null ? widget.pokemon.id.toString() : "?"}",
+                              style: const TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                            )
+                          ]),
+                    ),
+                    Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+                      Text(widget.pokemon.types?[0].type?.name?.capitalize() ?? ""),
+                      Text(widget.pokemon.types?.length == 2 ? widget.pokemon.types![1].type!.name!.capitalize() : "")
+                    ])
+                  ],
+                ),
+              ),
+            ),
+            Column(
+              children: [
+                Container(
+                  height: 125,
+                  width: 125,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    border: Border.all(
+                      color: Colors.white70,
+                      width: 10,
+                    ),
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ),
+                    boxShadow: const [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0.0, 1.5),
+                        blurRadius: 1.5,
+                      ),
+                    ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ),
+                    child: Image.network(
+                      widget.pokemon.sprites?.frontDefault ?? "",
+                      fit: BoxFit.cover,
+                      width: double.infinity,
+                      height: double.infinity,
+                    ),
+                  ),
+                )
+              ],
+            )
+          ],
+        )
+      ],
+    );
   }
 }
